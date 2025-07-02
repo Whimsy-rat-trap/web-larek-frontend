c0225997f3e1f5f1671a87b504e3c789306a633f
Whimsy <soulalex444@gmail.com>
2025-07-02 09:23:22 +0300

    Version 0.1.4 (changed interfaces within index.ts to be classes instead)

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,39 +1,39 @@
-export interface ProductCard {
-    id: string;                  // Уникальный идентификатор товара
-    title: string;               // Название товара
-    price: number | null;        // Цена товара (может быть null)
-    category: string;            // Категория товара
-    image: string;               // Путь к изображению (причина почему string: "/5_Dots.svg")
-    description?: string;         // Описание товара
+class Product {
+    id!: string;
+    image!: string;
+    title!: string;
+    category!: string;
+    price!: number | null;
+    description?: string;
 }
 
-export interface Cart {
-    items: CartItem[];           // Массив товаров в корзине
-    total: number;               // Общая сумма заказа
+class BasketItem {
+    productId!: string;
+    name!: string;
+    price!: number;
+    quantity = 1;
 }
 
-export interface CartItem {
-    productId: string;           // ID товара
-    name: string;                // Название товара (можно использовать title из Product)
-    price: number;               // Цена за единицу
-    quantity: number;            // Количество (по умолчанию 1)
+class Basket {
+    items!: BasketItem[];
+    total!: number;
 }
 
-export interface CheckoutForm {
-    payment: 'online' | 'on_delivery'; // Способ оплаты (соответствует API)
-    address: string;            // Адрес доставки
-    email: string;              // Email для уведомлений
-    phone: string;              // Контактный телефон
-    total: number;              // Итоговая сумма (добавлено согласно API)
-    items: string[];            // Массив ID товаров (соответствует API)
+class Order {
+    payment!: 'online' | 'on_delivery';
+    address!: string;
+    email!: string;
+    phone!: string;
+    total!: number;
+    items!: string[];
 }
 
-export interface SuccessfulCheckoutResponse {
-    id: string;                 // ID заказа (в API используется id)
-    total: number;              // Итоговая сумма
+class SuccessfulOrderResponse {
+    id!: string;
+    total!: number;
 }
 
-export interface ProductListResponse {
-    total: number;              // Общее количество товаров
-    items: ProductCard[];       // Массив товаров
+class ProductListResponse {
+    total!: number;
+    items!: Product[];
 }
