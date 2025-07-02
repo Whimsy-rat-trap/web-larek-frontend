b52518c3c30bd795bf97edf8c5c097f8fcb99da7
Whimsy <soulalex444@gmail.com>
2025-07-02 19:51:31 +0300

    V 0.1.91

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 import './scss/styles.scss';
+import { ensureElement } from './utils/utils';
+
+const productModal = ensureElement<HTMLElement>('#product-modal');
+const basketModal = ensureElement<HTMLElement>('#basket-modal');
+const paymentModal = ensureElement<HTMLElement>('#payment-modal');
+const contactsModal = ensureElement<HTMLElement>('#contacts-modal');
+const successModal = ensureElement<HTMLElement>('#success-modal');
+
